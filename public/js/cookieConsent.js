f7a727415d2c52046b847af20929caee918ba467
Davin Deol <davindeol@gmail.com>
2018-10-27 16:30:41 -0700

    Implemented animation

--- public/js/cookieConsent.js
+++ public/js/cookieConsent.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
         $("#invisibleBarBehindCookiePopup").height($('#cookiePopup').outerHeight());
     });
     $('#cookieConsent').on('click', function (e) {
-        $("#cookiePopup").remove();
+        $("#cookiePopup").slideUp('slow', function(){ $("#cookiePopup").remove(); });
+        $("#invisibleBarBehindCookiePopup").slideUp('slow', function(){ $("#invisibleBarBehindCookiePopup").remove(); });
         var d = new Date();
         d.setTime(d.getTime() + (365*24*60*60*1000));
         document.cookie = "CookieWarningDismissed=true;expires=" + d.toUTCString() + ";path=/";
