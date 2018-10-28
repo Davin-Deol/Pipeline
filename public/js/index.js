c741e2a77c39ad03b1c6df8e09cf06c6af87040d
Davin Deol <davindeol@gmail.com>
2018-10-27 18:00:26 -0700

    Fixed scroll down animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 $(function () {
     $('a').on('click', function (e) {
-        var offset = parseInt($($(this).attr('href')).offset().top);
+        var offset = parseInt($($(this).attr('href')).offset().top());
         var bheight = $(window).height();
         var percent = 0.07;
         var hpercent = bheight * percent;
